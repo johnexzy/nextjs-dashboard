ca5c843ee7aac0fe9c8e0fbde0c5d2b1f3b42d80
John Oba <obajohn75@gmail.com>
2023-11-03 11:48:42 +0100

    :sparkles: (next-navigation covered)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import '@/app/ui/global.css';
-import { lusitana, bangers } from '@/app/ui/fonts';
+import { lusitana, bangers, inter } from '@/app/ui/fonts';
 export default function RootLayout({
   children,
 }: {
@@ -7,7 +7,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${lusitana.variable} ${bangers.variable}`}>{children}</body>
+      <body className={`${inter.variable} ${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
